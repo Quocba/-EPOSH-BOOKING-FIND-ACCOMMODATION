b9c9b299772c753694a1aa238b72e753c2ec05f9
baodev <khanglam707@gmail.com>
2024-05-21 15:56:11 +0700

    Complete API: GetAll Hotel GetHotelByCIty GetHotelByID GetHotelByPrice

--- GraduationAPI-EPOSHBOOKING/IRepository/IHotelRepository.cs
+++ GraduationAPI-EPOSHBOOKING/IRepository/IHotelRepository.cs
@@ -5,5 +5,8 @@ namespace GraduationAPI_EPOSHBOOKING.IRepository
     public interface IHotelRepository
     {
         public ResponseMessage GetAllHotel();
+        public ResponseMessage GetHotelByCity(String city);
+        public ResponseMessage GetHotelByID (int hotelID);
+        public ResponseMessage GetHotelByPrice(double minPrice, double maxPrice);
     }
 }
---
+++ GraduationAPI-EPOSHBOOKING/Migrations/20240521074712_updateAvgRating.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GraduationAPI_EPOSHBOOKING.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateAvgRating : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "AvgRating",
+                table: "Hotel",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvgRating",
+                table: "Hotel");
+        }
+    }
+}
