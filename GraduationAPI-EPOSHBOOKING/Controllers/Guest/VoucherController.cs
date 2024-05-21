e83a7297fc4bc75b07bc2bc346a32ea595f8298c
Thanh Thợ Gõ <86115795+duythanhno1@users.noreply.github.com>
2024-05-21 15:47:11 +0700

    Get blog and get

---
+++ GraduationAPI-EPOSHBOOKING/Controllers/Guest/VoucherController.cs
@@ -0,0 +1,40 @@
+﻿using GraduationAPI_EPOSHBOOKING.IRepository;
+using GraduationAPI_EPOSHBOOKING.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace GraduationAPI_EPOSHBOOKING.Controllers.Guest
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VoucherController : ControllerBase
+    {
+        private readonly IVoucherRepository _voucherRepository;
+
+        public VoucherController(IVoucherRepository voucherRepository)
+        {
+            _voucherRepository = voucherRepository ?? throw new ArgumentNullException(nameof(voucherRepository));
+        }
+
+        [HttpGet]
+        public IActionResult GetAllVouchers()
+        {
+            try
+            {
+                var response = _voucherRepository.GetAllVouchers();
+                return StatusCode(response.StatusCode, response);
+            }
+            catch (Exception ex)
+            {
+                // Xử lý lỗi và trả về phản hồi lỗi nếu cần
+                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseMessage
+                {
+                    Success = false,
+                    Message = "Internal Server Error",
+                    StatusCode = StatusCodes.Status500InternalServerError
+                });
+            }
+        }
+    }
+}
---
+++ GraduationAPI-EPOSHBOOKING/Repository/BlogRepository.cs
@@ -0,0 +1,45 @@
+﻿using GraduationAPI_EPOSHBOOKING.DataAccess;
+using GraduationAPI_EPOSHBOOKING.IRepository;
+using GraduationAPI_EPOSHBOOKING.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace GraduationAPI_EPOSHBOOKING.Repository
+{
+    public class BlogRepository : IBlogRepository
+    {
+        private readonly DBContext _dbContext;
+
+        public BlogRepository(DBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public ResponseMessage GetAllBlogs()
+        {
+            try
+            {
+                var blogs = _dbContext.blog
+                    .Include(b => b.Comment)
+                    .Include(b => b.BlogImage)
+                    .ToList();
+
+                if (blogs != null && blogs.Any())
+                {
+                    return new ResponseMessage { Success = true, Message = "Successfully", Data = blogs, StatusCode = (int)HttpStatusCode.OK };
+                }
+                else
+                {
+                    return new ResponseMessage { Success = false, Message = "No blogs found", StatusCode = (int)HttpStatusCode.NotFound };
+                }
+            }
+            catch (Exception ex)
+            {
+                // Xử lý các trường hợp ngoại lệ nếu cần
+                return new ResponseMessage { Success = false, Message = ex.Message, StatusCode = (int)HttpStatusCode.InternalServerError };
+            }
+        }
+    }
+}
