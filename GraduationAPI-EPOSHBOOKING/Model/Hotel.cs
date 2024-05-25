64e2b4ed16716d79f66e3a3b235de87a6c5d4dce
baodev <khanglam707@gmail.com>
2024-05-25 20:11:30 +0700

    Commit Everytime

--- GraduationAPI-EPOSHBOOKING/Model/Hotel.cs
+++ GraduationAPI-EPOSHBOOKING/Model/Hotel.cs
@@ -16,7 +16,8 @@ namespace GraduationAPI_EPOSHBOOKING.Model
         public String Description { get; set; }
 
         public int HotelStandar { get; set; }
-        public bool Status { get; set;}
+        public bool isActive { get; set;}
+        public String isRegister { get; set; }
         [ForeignKey("AccountID")]
         public Account Account { get; set; }
 
