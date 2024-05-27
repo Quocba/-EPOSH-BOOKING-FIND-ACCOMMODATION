08dab5cba2d18e87f2580e94085fa6c961d75541
baodev <khanglam707@gmail.com>
2024-05-27 13:45:00 +0700

    Add API: Cancle Booking

---
+++ GraduationAPI-EPOSHBOOKING/Migrations/20240527062418_updateBookingReasonCancle.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GraduationAPI_EPOSHBOOKING.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateBookingReasonCancle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+            name: "ReasonCancle",
+            table: "Booking",
+            type: "nvarchar(max)",
+            nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+        name: "ReasonCancle",
+        table: "Booking");
+        }
+    }
+}
